007cebe48bd84fd993649cacf855f2b82198225a
Pierre Fournier <pierre@fournier.net>
2024-01-20 23:34:06 +0100

    mod(client&server): removed bending and added yuusha townscape

--- generate-modlist.sh
+++ generate-modlist.sh
@@ -2,14 +2,14 @@ mkdir -p tmp/{server,client}
 cd client
 packwiz modrinth export -o The-InBetween-Client.mrpack
 cd ../tmp/client
-rustup run stable mcman init --mrpack ../../client/The-InBetween-Client.mrpack --name The-InBetween-Client
-rustup run stable mcman markdown
+mcman init --mrpack ../../client/The-InBetween-Client.mrpack --name The-InBetween-Client
+mcman markdown
 cat README.md > ../../CLIENT.md
 cd ../../server
 packwiz modrinth export -o The-InBetween-Server.mrpack
 cd ../tmp/server
-rustup run stable mcman init --mrpack ../../server/The-InBetween-Server.mrpack --name The-InBetween-Server
-rustup run stable mcman markdown
+mcman init --mrpack ../../server/The-InBetween-Server.mrpack --name The-InBetween-Server
+mcman markdown
 cat README.md > ../../SERVER.md
 cd ../..
 rm -rf tmp
